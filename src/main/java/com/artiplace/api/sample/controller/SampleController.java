ec5374ba4cfdbc28679355731e5ce22fd1851049
201402407 <banner4@naver.com>
2021-05-04 16:46:07 +0900

    SERVER CORS filter 설정

--- src/main/java/com/artiplace/api/sample/controller/SampleController.java
+++ src/main/java/com/artiplace/api/sample/controller/SampleController.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;

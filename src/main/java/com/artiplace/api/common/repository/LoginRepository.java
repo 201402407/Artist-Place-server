47fc676b5a85c3ed3c7e50d03b6d8a0dae0982d5
201402407 <banner4@naver.com>
2021-05-16 18:50:50 +0900

    JPA 서버 통신 가능, 서버 가동 완료

--- src/main/java/com/artiplace/api/common/repository/LoginRepository.java
+++ src/main/java/com/artiplace/api/common/repository/LoginRepository.java
@@ -4,10 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.artiplace.api.common.entity.LoginEntity;
-import com.artiplace.api.common.pvo.LoginPVO;
 	
 @Repository
-public interface LoginRepository extends JpaRepository<LoginPVO, String> {
+public interface LoginRepository extends JpaRepository<LoginEntity, String> {
 
 	LoginEntity findByEmailId(String emailId);
 }

1d8495592cbb295c556d129b46be551da3a3505f
201402407 <banner4@naver.com>
2021-05-23 17:39:52 +0900

    CORS 내가 뿌리 뽑는다 진짜.. 왜 데스크톱만 되는건데..

--- src/main/java/com/artiplace/api/comn/util/SimpleCORSFilter.java
+++ src/main/java/com/artiplace/api/comn/util/SimpleCORSFilter.java
@@ -27,18 +27,32 @@ public SimpleCORSFilter() {
 }
 
 @Override
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 
     HttpServletRequest request = (HttpServletRequest) req;
     HttpServletResponse response = (HttpServletResponse) res;
 
+    if(request.getHeader("Origin") == null) {
+    	System.out.println("NULL");
+    }
+    else {
+    	System.out.println(request.getHeader("Origin"));
+    }
+//    response.setHeader("Access-Control-Allow-Origin", "*");
     response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
     response.setHeader("Access-Control-Allow-Credentials", "true");
     response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
     response.setHeader("Access-Control-Max-Age", "3660");
     response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With");
+//    response.setHeader("Access-Control-Allow-Headers", "*");
 
-    chain.doFilter(req, res);
+//    if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+//        response.setStatus(HttpServletResponse.SC_OK);
+//    } else {
+//        chain.doFilter(req, res);
+//    }
+    chain.doFilter(req, res);	
 }
 
 @Override
